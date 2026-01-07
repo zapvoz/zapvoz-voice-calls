f0563eb8c1704a7e3ab09a41f871910bb3966213
zapvoz <contato@zapvoz.com>
2026-01-07 19:08:03 -0300

    chore: update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export { useZapVozVoiceCalls, ZapVozVoiceCalls } from './services/transport.model';
+export { CallSignaling } from './services/call-signaling';
 export type { 
   ZapVozOptions, 
   CallEvent, 
   ConnectionStatus,
-  ZapVozEventMap 
+  ZapVozEventMap,
+  CallOffer,
+  OutgoingCallOptions
 } from './services/transport.type';
